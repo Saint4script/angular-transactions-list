2190afdc67f8b8009fac6cc9595991637b7da915
KS <admin3@igcmd.ru>
2021-11-28 19:10:30 +0800

    component fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListPageComponent } from './list-page/list-page.component';
+import { SummaryComponent } from './summary/summary.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: SummaryComponent},
+    { path: 'list-page', component: ListPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
